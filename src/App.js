0e46b6180eb31c0ba87469e1923fc678c501a545
Juan Delmuro <jgmuro@amazon.com>
2023-04-16 11:58:20 -0400

    Minor Change

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          V2
+          V3
         </p>
         <a
           className="App-link"
