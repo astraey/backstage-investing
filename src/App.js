6373ebd9526952e3026d4feee5f116c4fbeaef6b
Juan Delmuro <jgmuro@amazon.com>
2023-04-17 19:43:55 -0400

    Removed Top Page Logo

--- src/App.js
+++ src/App.js
@@ -20,10 +20,6 @@ import {
 const App = ({ signOut }) => (
     <div>
       <View className="App">
-        <Card>
-          <Image src={logo} className="App-logo" alt="logo" />
-          <Heading level={1}>We now have Auth!</Heading>
-        </Card>
         <Button onClick={signOut}>Sign Out</Button>
       </View>
       <ThemeCustomization>
