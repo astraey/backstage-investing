938e469506e90b5f839885a66a028bc678a6ef00
Juan Delmuro <jgmuro@amazon.com>
2023-04-26 17:25:01 -0400

    Draft Chart Added

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,7 @@
 import logo from 'assets/images/icons/backstage.svg';
 
-
 const Logo = () => {
-  return (
-    <img src={logo} alt="Mantis" width="150" />
-  );
+  return <img src={logo} alt="Mantis" width="150" />;
 };
 
 export default Logo;
--- src/pages/Company.js
+++ src/pages/Company.js
@@ -1,6 +1,7 @@
 import { API } from 'aws-amplify';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Chart from 'react-apexcharts';
 
 const apiName = 'santamariaapi';
 const requestVariables = {
@@ -14,35 +15,69 @@ const requestVariables = {
 const Company = () => {
   const [dataFromAPI, setDataFromAPI] = useState(null);
   const [grossProfit, setGrossProfit] = useState(null);
+  const [fiscalDateEnding, setFiscalDateEnding] = useState(null);
+  const [inputChartData, setInputChartData] = useState(null);
+  const [inputChartDataOptions, setInputChartDataOptions] = useState(null);
+  const [inputChartDataSeries, setInputChartDataSeries] = useState(null);
+  const [dataReceived, setDataReceived] = useState(null);
+
   const params = useParams();
-  //const grossProfit = [];
+
   useEffect(() => {
     let path = `/company/${params.companyTicker}`;
     let grossProfit = [];
+    let fiscalDateEnding = [];
     setDataFromAPI();
-    API.get(apiName, path, requestVariables)
-      .then((response) => {
-        response.quarterlyReports.map((quarterlyReport) => {
-          //console.log(quarterlyReport.grossProfit);
-          grossProfit.push(quarterlyReport.grossProfit)
-        });
-        // Add your code here
-        console.log(grossProfit);
-        console.log(response);
-        setDataFromAPI(response);
-        setGrossProfit(grossProfit);
-      })
-      .catch((error) => {
-        console.log(error.response);
+    API.get(apiName, path, requestVariables).then((response) => {
+      console.log(response);
+      response.quarterlyReports.map((quarterlyReport) => {
+        //console.log(quarterlyReport.fiscalDateEnding);
+        grossProfit.push(`${quarterlyReport.grossProfit / 1000000}M`);
+        //grossProfit.push(quarterlyReport.grossProfit);
+        fiscalDateEnding.push(quarterlyReport.fiscalDateEnding);
       });
+      // Add your code here
+      setDataFromAPI(response);
+
+    setInputChartDataOptions({
+      chart: {
+        id: 'basic-bar',
+      },
+      xaxis: {
+        categories: fiscalDateEnding,
+      },
+    });
+    setInputChartDataSeries([
+      {
+        name: 'series-1',
+        data: grossProfit,
+      },
+    ])
+
+    setDataReceived(true);
+    
+
+  
+  
+    })
+    .catch((error) => {
+      console.log(error.response);
+    });
+  
   }, []);
+
   return (
     <div>
       <h1>{params.companyTicker} Page</h1>
-      <p>Company Data (Coming from our API)</p>
-      <p>{JSON.stringify(grossProfit)}</p>
-      <p>{JSON.stringify(dataFromAPI)}</p>
+      <p>{params.companyTicker} Gross Profit (Coming from our API)</p>
+      {dataReceived
+        ? <Chart options={inputChartDataOptions} series={inputChartDataSeries} type="bar" width="500" />
+        : <p>Loading...</p>
+      }
+      <p>{JSON.stringify(inputChartDataSeries)}</p>
+      <p>{JSON.stringify(inputChartDataOptions)}</p>
     </div>
   );
 };
 export default Company;
+
