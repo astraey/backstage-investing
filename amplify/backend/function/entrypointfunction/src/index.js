9ae3c2cfb47e0611d61feb43768d9700e4d1ffe7
Juan Delmuro <jgmuro@amazon.com>
2023-06-10 13:19:58 -0400

    YH Path Added to API

--- amplify/backend/function/entrypointfunction/src/index.js
+++ amplify/backend/function/entrypointfunction/src/index.js
@@ -9,14 +9,26 @@ const headers = {
   'Access-Control-Allow-Headers': '*',
 };
 exports.handler = async (event) => {
+  console.log(event.resource);
   if (typeof event.resource !== 'undefined') {
-    switch (true) {
-      case event.resource.includes('company'):
-        return {
-          statusCode: 200,
-          headers: headers,
-          body: JSON.stringify(await alphaVantageRequestHandler.getCompanyMetrics(event.pathParameters.proxy)),
-        };
+    if (event.resource.includes('/company/')) {
+      console.log("We are executing the original Company API Request");
+      return {
+        statusCode: 200,
+        headers: headers,
+        body: JSON.stringify(await alphaVantageRequestHandler.getCompanyMetrics(event.pathParameters.proxy)),
+      };
+    }
+    if (event.resource.includes('/yhcompany/')) {
+      console.log("We are executing the new Yahoo Company API Request");
+      return {
+        statusCode: 200,
+        headers: headers,
+        body: JSON.stringify({
+          event: event || {},
+          info: 'We are executing the new Yahoo Company API Request',
+        }),
+      };
     }
   } else {
     return {
